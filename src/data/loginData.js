2571a3c99aca9e556a4dd45fc607db12a264ab26
Ngominhdong99 <Minhdong20499@gmail.com>
2022-04-14 14:00:48 +0700

    first commit

---
+++ src/data/loginData.js
@@ -0,0 +1,22 @@
+export const data = [
+  {
+    id: 1,
+    userName: "dongnm",
+    password: 12345678,
+  },
+  {
+    id: 2,
+    userName: "hungdnv",
+    password: 12345678,
+  },
+  {
+    id: 1,
+    userName: "chaunv",
+    password: 12345678,
+  },
+  {
+    id: 1,
+    userName: "huynq",
+    password: 12345678,
+  },
+];
